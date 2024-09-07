ece098a0d88f1428bf5b660131a3e2158bcdb761
KyleMcIndoe <kmcindoe06@gmail.com>
2024-09-07 17:53:07 -0400

    Fen string parse piece positions

--- ChessParser/Scripts.cs
+++ ChessParser/Scripts.cs
@@ -1,3 +1,31 @@
 ﻿namespace ChessParser {
 
+    public static class funcs {
+        public static string returnField(string s, int x) { // return one field of the fen string
+            string[] fields = s.Split(" ");
+            return fields[x];
+        }
+    }
+    public static class parses {
+        static char[,] fenPositions(string fenString) { // fill board with nothing but positions of pieces
+            char[,] board = new char[8,8];
+
+            string piecePositions = funcs.returnField(fenString, 0);
+            string[] rows = piecePositions.Split("/");
+
+            for(int i = 0; i < 8; i++) {
+                char[] charArr = rows[i].ToCharArray(0, rows[i].Length);
+
+                for(int j = 0; j < charArr.Length; j++) {
+                    if(Char.IsDigit(charArr[j])) {
+                        i += (int) charArr[j] - 48; // (int) returns the number + 48
+                    } else {
+                        board[i,j] = charArr[i];
+                    }
+                }
+            }
+
+            return board;
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ChessParser
+
+Project built to take fen strings and other notation, and translate them into a readable chess board.
+
+Though this may change later, parses return a 2d array of chars.
+
+char array follows fen notation piece representation
+- Lower case char represents a black piece
+- Upper case char represents a white piece
+
+- p = pawn
+- r = rook
+- n = knight (k is for the king)
+- b = bishop
+- q = queen
+- k = da king
